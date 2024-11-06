dfe1f257a8b1b2e8101da735bca785883de58019
tea <lazzarinilogan@gmail.com>
2024-11-05 21:48:27 -0800

    please work

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = requires("cors");
+
+const app = express();
+
+app.use(express.json);
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+app.listen(port, (req, res) => {
+    console.log('Server running on port: ${port}');
+});
