3a198e3d584991e572fbe94535abd7087bd94a64
Shubhan Yenuganti <yrshubhan@gmail.com>
2024-11-07 17:07:49 -0800

    Created selenium scripts that pull occupancy data from BFit and Wooden. Next goal is to create an additional backend server (MongoDB or something else) that converts the pulled data into an API for our frontend.

---
+++ backend/selenium_scripts/bfit_pull.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def fetch_data():
+    url = "https://recreation.ucla.edu/facilities/bfit"
+    driver = webdriver.Chrome()
+
+    driver.get(url)
+
+    frame = driver.find_elements(By.TAG_NAME, "iframe")[0]
+
+    driver.switch_to.frame(frame)
+
+    bars = driver.find_elements(By.CLASS_NAME, "barChart")
+
+    data_list = []
+
+    for bar in bars:
+        text_lines = bar.text.split('\n')
+        
+        if len(text_lines) >= 5:
+            place_data = {
+                "place_name": text_lines[0],          # First line: Place name
+                "status": text_lines[1],              # Second line: Status
+                "last_count": text_lines[2].split(": ")[1],  # Third line: Last count
+                "updated_time": text_lines[3].split(": ")[1], # Fourth line: Updated time
+                "percentage": text_lines[4]           # Fifth line: Percentage
+            }
+            data_list.append(place_data)
+
+    driver.quit()
+    print(data_list)
+
+fetch_data()
