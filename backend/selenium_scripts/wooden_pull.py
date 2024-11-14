42fb3f1396bc39fcf17d2a3d6a8ef2f57b9bc1a0
Nolan Quiroz <nolanquiroz@Nolans-MacBook-Pro.local>
2024-11-13 20:07:29 -0800

    Separated scripts for testing purposes. Meant to improve compatibility with store_data.py.

---
+++ backend/selenium_scripts/wooden_pull.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+
+
+def fetch_data(logic):
+    url = "https://recreation.ucla.edu/facilities/jwc"
+
+    # Code for selenium's web scraper (magic)
+    driver = webdriver.Chrome()
+    driver.get(url)
+    frame = driver.find_elements(By.TAG_NAME, "iframe")[0]
+    driver.switch_to.frame(frame)
+    bars = driver.find_elements(By.CLASS_NAME, "barChart")
+
+    # Return array of data
+    data_list = []
+
+    # Collect data in graph
+    for bar in bars:
+        text_lines = bar.text.split('\n')
+        
+        if len(text_lines) >= 5:
+            place_data = {
+                "place_name": text_lines[0],          # First line: Place name
+                "status": text_lines[1],              # Second line: Status
+                "last_count": text_lines[2].split(": ")[1],  # Third line: Last count
+                "updated_time": text_lines[3].split(": ")[1], # Fourth line: Updated time
+                "percentage": text_lines[4]           # Fifth line: Percentage
+            }
+            data_list.append(place_data)
+
+    driver.quit()
+
+    # Returns in json format
+    return json.dumps(data_list)
