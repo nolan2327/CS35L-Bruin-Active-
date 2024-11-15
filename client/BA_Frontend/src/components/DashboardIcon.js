d34335a2e2bccf93f6aada9be3d3ea5591fcc544
Nolan Quiroz <nolanquiroz@Nolans-MacBook-Pro.local>
2024-11-14 19:47:39 -0800

    Frontend skeleton with working buttons & icons, trying out special fonts for header. Dynamic data not yet implemented.

---
+++ client/BA_Frontend/src/components/DashboardIcon.js
@@ -0,0 +1,14 @@
+
+// DashboardIcon.js
+
+import React from 'react';
+
+const DashboardIcon = () => {
+  return (
+    <span className="material-symbols-outlined icon-hover">
+      dashboard
+    </span>
+  );
+};
+
+export default DashboardIcon;
