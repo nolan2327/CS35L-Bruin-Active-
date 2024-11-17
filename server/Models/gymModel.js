c74b4d60aabd3bb63ac625e74e9cb9b4be44789a
Shubhan Yenuganti <yrshubhan@gmail.com>
2024-11-17 14:49:24 -0800

    Created backend model for gym that consists of name, description, hours and occupancy

--- server/Models/gymModel.js
+++ server/Models/gymModel.js
@@ -1,16 +1,85 @@
 const mongoose = require("mongoose");
 
-const gymSchema = new mongoose.Schema( // not yet correct after this
-    {
-        name: { type: String, required: true, minlength: 3, maxlength: 30},
-        email: { type: String, required: true, minlength: 3, maxlength: 200, unique: true},
-        password: { type: String, required: true, minlength:3 , maxlength: 1024},
+const gymSchema = new mongoose.Schema( 
+   {
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
-    {
-        timestamps: true,
-    }
+    description: {
+        type: String,
+        trim: true
+    },
+    hours: {
+        defaultHours: {
+            mondayToThursday: String,
+            friday: String,
+            saturday: String,
+            sunday: String,
+        },
+        specialHours: [
+            {
+                dates: {
+                    type: String,
+                    required: true
+                },
+                hours: {
+                    type: String,
+                    required: true
+                }
+            }
+        ]
+    },
+    facilities: [
+        {
+            name: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            occupancy: {
+                capacity: {
+                    type: Number,
+                    required: true,
+                    min: 0
+                },
+                current: {
+                    type: Number,
+                    required: true,
+                    min: 0
+                }
+            }
+        }
+    ]
+   }
 );
 
-const gymModel = mongoose.model("Gym", gymSchema);
+gymSchema.pre('save', function (next) {
+    const gym = this;
+
+    if (!gym.hours.defaultHours) {
+        if (gym.name === 'John Wooden Center') {
+            gym.hours.defaultHours = {
+                mondayToThursday: '5:15 AM - 1:00 AM',
+                friday: '5:15 AM - 10:00 PM',
+                saturday: '8:00 AM - 8:00 PM',
+                sunday: '8:00 AM - 11:00 PM',
+            };
+        } else if (gym.name === 'Bruin Fitness Center') {
+            gym.hours.defaultHours = {
+                mondayToThursday: '6:00 AM - 1:00 AM',
+                friday: '6:00 AM - 9:00 PMM',
+                saturday: '9:00 AM - 6:00 PM',
+                sunday: '9:00 AM - 6:00 PM',
+            };
+        }
+    }
+
+    next();
+});
+
+const gymModel = mongoose.model('Gym', gymSchema);
+
 
 module.exports = gymModel;
