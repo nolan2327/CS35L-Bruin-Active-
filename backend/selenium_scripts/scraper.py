36719210fc6749388b3dab0dcf8526f2951dc320
Shubhan Yenuganti <yrshubhan@gmail.com>
2024-11-17 16:46:57 -0800

    Added ability to scrape special hours for BFit.

--- backend/selenium_scripts/scraper.py
+++ backend/selenium_scripts/scraper.py
@@ -36,33 +36,60 @@ def fetch_data(logic):
             }
             data_list.append(place_data)
 
+    driver.switch_to.default_content()
     time_pattern = r'\b\d{1,2}:\d{2}\s?[AP]M\b\s?-\s?\d{1,2}:\d{2}\s?[AP]M\b'
+    date_pattern = r'^([\d/]+(?:-[\d/]+)?),?\s+([\w-]+)'
+    days_pattern = r'^[A-Za-z]+(?:\s*-\s*[A-Za-z]+)?'
+
     # Extract Regular Hours 
     if (logic == 1):        
-        driver.switch_to.default_content()
         regular_hours = driver.find_elements(By.CLASS_NAME, "callout")[1]
-        regular_hours = regular_hours.text.split('\n')[1:5]
-        times = [re.search(time_pattern, hour).group() for hour in regular_hours]
-        regular_hours_data = {
-            "mondayToThursday": times[0],
-            "friday": times[1],
-            "saturday": times[2],
-            "sunday": times[3]
-        }
-        data_list.append(regular_hours_data)
     else:
-        driver.switch_to.default_content()
         regular_hours = driver.find_elements(By.CLASS_NAME, "callout")[0]
-        regular_hours = regular_hours.text.split('\n')[1:5]
-        times = [re.search(time_pattern, hour).group() for hour in regular_hours]
-        regular_hours_data = {
-            "mondayToThursday": times[0],
-            "friday": times[1],
-            "saturday": times[2],
-            "sunday": times[3]
-        }
-        data_list.append(regular_hours_data)
 
+    regular_hours = regular_hours.text.split('\n')[1:5]
+    times = [re.search(time_pattern, hour).group() for hour in regular_hours]
+    regular_hours_data = {
+        "mondayToThursday": times[0],
+        "friday": times[1],
+        "saturday": times[2],
+        "sunday": times[3]
+    }
+    data_list.append(regular_hours_data)
+
+    # Extract Special Hours
+    if (logic != 1):
+        special_hours_element = driver.find_elements(By.CLASS_NAME, "column.column-2")[0]
+        special_hours_lines = special_hours_element.text.split('\n')
+        special_hours = special_hours_lines[7:11]
+        finals_date = special_hours_lines[11]
+        finals_hours = special_hours_lines[12:16]
+
+        times_special = [
+            (match.group() if (match := re.search(time_pattern, hour)) else "Closed")
+            for hour in special_hours
+        ]
+
+        dates_and_weekdays_special = [
+            f"{match.group(1)}, {match.group(2)}"
+            for line in special_hours if (match := re.search(date_pattern, line))
+        ]
+
+        special = dict(zip(dates_and_weekdays_special, times_special))
+        data_list.append(special)
+
+        times_finals = [
+            (match.group() if (match := re.search(time_pattern, hour)) else "Closed")
+            for hour in finals_hours
+        ]
+
+        days_finals = [re.match(days_pattern, line).group() for line in finals_hours if re.match(days_pattern, line)]
+
+        finals = dict(zip(days_finals, times_finals))
+        data_list.append({"Finals Week Date": finals_date})
+        data_list.append(finals)    
+    # To do (else statement for wooden)
+     
     driver.quit()
 
     # Returns in json format
