86062a3d33244a04c3f6949f6c6a39758d6842b0
tea <lazzarinilogan@gmail.com>
2024-11-20 20:11:29 -0800

    adding functions to profileController

--- server/Routes/profileRoute.js
+++ server/Routes/profileRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const {createProfile, findProfile, findProfilesByName, getProfiles} = require("../Controllers/profileController");
+const {createProfile, findProfile, findProfilesByName, changeStatus, changeBio, getProfiles} = require("../Controllers/profileController");
 
 const router = express.Router();
 
 router.post("/createProfile", createProfile);
 router.get("/findProfile", findProfile);
 router.get("/find", findProfilesByName)
+router.post("/changeStatus", changeStatus);
+router.post("/changeBio", changeBio);
 router.get("/", getProfiles);
 
 module.exports = router;
