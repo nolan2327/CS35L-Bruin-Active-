5c0cc03e57ddf1f3be516e2da7cdd78c38da70cf
tea <lazzarinilogan@gmail.com>
2024-11-20 19:44:14 -0800

    removed extra require and removed port from .env

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+
+// Routes
 const userRoute = require("./Routes/userRoute");
 // const gymRoute = require("./Routes/gymRoute");
 const profileRoute = require("./Routes/profileRoute");
 const calendarRoute = require("./Routes/calendarRoute");
-const db = require("./Config/db");
+
 const connectDB = require("./Config/db");
 // const { MongoClient } = require("mongodb");
 
@@ -19,16 +21,11 @@ app.use("/api/users", userRoute);
 app.use("api/profiles", profileRoute);
 app.use("api/calendar", calendarRoute);
 
-app.get("/", (req, res) => {
-    res.send("Welcome to bruin active");
-});
-
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; // Uses port 5000 unless other port is given, don't add port to .env file
 const uri = process.env.ATLAS_URI;
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+    console.log(`Server connecting on port: ${port}`);
 });
 
 connectDB(uri);
