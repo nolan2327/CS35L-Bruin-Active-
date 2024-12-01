d592ab95e4c7c84baa5acb6a9be521a94e698e15
Joey Castaneda <joeyscastaneda@gmail.com>
2024-12-01 09:30:06 -0800

    Added two icons for the profile page

---
+++ client/BA_Frontend/src/components/EditIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditIcon = () => {
+    return (
+        <span className="material-symbols-outlined icon-hover">
+            edit
+        </span>
+    );
+};
+
+export default EditIcon;
---
+++ client/BA_Frontend/src/components/LogoutIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogoutIcon = () => {
+    return (
+        <span className="material-symbols-outlined icon-hover">
+            logout
+        </span>
+    );
+};
+
+export default LogoutIcon;
