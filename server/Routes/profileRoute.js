ade46a657372280c6d84efd139f30d318fd155c8
Shubhan Yenuganti <yrshubhan@gmail.com>
2024-12-06 18:49:59 -0800

    Removed unecessary imports.

--- server/Routes/profileRoute.js
+++ server/Routes/profileRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {createProfile, findProfile, findProfilesByName, changeStatus, changeBio, getProfiles} = require("../Controllers/profileController");
+const {createProfile, findProfile, changeStatus, changeBio, getProfiles} = require("../Controllers/profileController");
 
 const router = express.Router();
 
@@ -9,4 +9,4 @@ router.post("/changeStatus", changeStatus);
 router.post("/changeBio", changeBio);
 router.get("/", getProfiles);
 
-module.exports = router;
+module.exports = router
